d157ac4dbe9451741d592c7c08e11e0dfd99bec8
Shuntaro Nishizawa <me@shun.technology>
2021-05-09 13:32:44 +0900

    feat: add graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-import { hello } from "./hello";
+import { fastify } from "fastify";
+import mercurius from "mercurius";
+import * as fs from "fs/promises";
+import * as path from "path";
 
-console.log(hello("world"));
+import type { Resolvers } from "./generated/graphql";
+
+async function main(): Promise<void> {
+  const server = fastify();
+
+  const schema = await loadSchema();
+
+  const resolvers: Resolvers = {
+    Query: {
+      ok: async () => true,
+    },
+  };
+
+  server.register(mercurius, {
+    graphiql: "playground",
+    schema,
+    resolvers,
+  });
+
+  await server.listen(3000);
+  console.log("listen on http://localhost:3000");
+}
+
+function loadSchema(): Promise<string> {
+  const schemaPath = path.join(__dirname, "..", "schema.graphql");
+
+  return fs.readFile(schemaPath, "utf-8");
+}
+
+main().catch(console.error);
