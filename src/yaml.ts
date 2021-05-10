12f2cf300a57790a7c0d50174c01f4a1149daafb
Shuntaro Nishizawa <me@shun.technology>
2021-05-10 22:10:24 +0900

    feat: add to read yaml

---
+++ src/yaml.ts
@@ -0,0 +1,51 @@
+import YAML from "yaml";
+import * as fs from "fs/promises";
+import * as path from "path";
+
+export interface Information {
+  basic: Basic;
+  skills: Array<SkillCategory>;
+  histories: Array<History>;
+  contacts: Array<Contact>;
+}
+
+interface Basic {
+  name: string;
+  aka: string;
+  birthday: string;
+  school: string;
+  department: string;
+}
+
+interface SkillCategory {
+  name: string;
+  skills: Array<Skill>;
+}
+
+interface Skill {
+  name: string;
+  icon?: string;
+}
+
+interface History {
+  date: string;
+  title: string;
+  links?: Array<Link>;
+}
+
+interface Link {
+  name: string;
+  url: string;
+}
+
+interface Contact {
+  name: string;
+  uri: string;
+  icon?: string;
+}
+
+export async function readInformation(): Promise<Information> {
+  const filePath = path.join(__dirname, "..", "info.yml");
+  const rawFile = await fs.readFile(filePath, "utf-8");
+  return YAML.parse(rawFile);
+}
